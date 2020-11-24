e97567bfc59e74b59a5fb2a2ddc251d2153c7cdf
PAPADOXIE <nofilqasim@gmail.com>
2020-11-25 02:24:12 +0500

    Made compile script

---
+++ Program/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+g++ ./source/main.cpp -o ./bin/VirusSimulator \
+./source/City.cpp \
+./source/Clean.cpp \
+./source/FileHandler.cpp \
+./source/Human.cpp \
+./source/HumanList.cpp \
+./source/Infected.cpp \
+./source/Randomizer.cpp \
+./source/Screen.cpp \
+./source/Time.cpp
